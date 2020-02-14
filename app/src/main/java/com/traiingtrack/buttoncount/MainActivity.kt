e670aa6aa3f7556281e8c77ca1041f00be176eaa
Derfel Cadarn <gehigbie@gmail.com>
2020-02-13 21:53:35 -0700

    Add vars

--- app/src/main/java/com/traiingtrack/buttoncount/MainActivity.kt
+++ app/src/main/java/com/traiingtrack/buttoncount/MainActivity.kt
@@ -2,8 +2,17 @@ package com.traiingtrack.buttoncount
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var editText: EditText? = null;
+    private var button: Button? = null;
+    private var textView: TextView? = null;
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
