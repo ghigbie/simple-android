0c96e706e3490c6be1417df6d4de136baa32b1f4
Derfel Cadarn <gehigbie@gmail.com>
2020-02-14 21:37:56 -0700

    Add lifcyle methods

--- app/src/main/java/com/traiingtrack/buttoncount/MainActivity.kt
+++ app/src/main/java/com/traiingtrack/buttoncount/MainActivity.kt
@@ -2,23 +2,26 @@ package com.traiingtrack.buttoncount
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
+private val TAG: String = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
 
     private var textView: TextView? = null;
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var userInput: EditText = findViewById<EditText>(R.id.userInput)
-        var button: Button = findViewById<Button>(R.id.button)
+        val userInput: EditText = findViewById<EditText>(R.id.userInput)
+        val button: Button = findViewById<Button>(R.id.button)
         textView = findViewById<TextView>(R.id.editTextMain)
         var numTimesClicked: Int = 0
         var name: String = ""
@@ -29,7 +32,8 @@ class MainActivity : AppCompatActivity() {
 
         button?.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
-            val toast: Toast = Toast.makeText(applicationContext, "You tapped me, ouch!", Toast.LENGTH_SHORT);
+                Log.d(TAG, "onClick called")
+                val toast: Toast = Toast.makeText(applicationContext, "You tapped me, ouch!", Toast.LENGTH_SHORT);
                 numTimesClicked++;
                 name = userInput.text.toString()
                 val s: String = if(numTimesClicked > 1) "s" else ""
@@ -39,5 +43,36 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart called")
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d(TAG, "onRestoreInstanceState called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
     }
 }
